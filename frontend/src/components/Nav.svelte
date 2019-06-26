c4209e5538b942890dd57a3f99bd29bd4cb01092
Paulmicha <paul.michalet@gmail.com>
2019-06-26 12:54:46 -0300

    feat: main menu nav

--- frontend/src/components/Nav.svelte
+++ frontend/src/components/Nav.svelte
@@ -1,60 +1,37 @@
 <script>
-	export let segment;
+  import IconMenu from "./icons/IconMenu.svelte";
+  export let segment;
+
+  // TODO fetch from Drupal menu via REST API.
+  const menu_links = [
+    { path: "articles", label: "Articles" },
+    { path: "recipes", label: "Recipes" }
+  ];
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	.selected {
-		position: relative;
-		display: inline-block;
-	}
-
-	.selected::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
+  button {
+    display: block;
+    min-height: 1em;
+    min-width: 1em;
+  }
 </style>
 
 <nav>
-	<ul>
-		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
-		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
-	</ul>
+  <ul>
+    {#each menu_links as l}
+      <li>
+        <a class="font-serif {segment === l.path ? 'is-on' : ''}" href={l.path}>
+           {l.label}
+        </a>
+      </li>
+    {/each}
+  </ul>
+  <button
+    aria-label="Toggle the menu"
+    on:click={e => {
+      console.log(e);
+    }}>
+    <IconMenu />
+  </button>
 </nav>
---
+++ frontend/src/components/icons/IconMenu.svelte
@@ -0,0 +1,35 @@
+<style>
+  .icon-menu {
+    color: #000;
+    position: absolute;
+    margin-left: 2px;
+    margin-top: 10px;
+    width: 17px;
+    height: 1px;
+    background-color: currentColor;
+  }
+
+  .icon-menu:before {
+    content: "";
+    position: absolute;
+    top: -5px;
+    left: 0;
+    width: 17px;
+    height: 1px;
+    background-color: currentColor;
+  }
+
+  .icon-menu:after {
+    content: "";
+    position: absolute;
+    top: 5px;
+    left: 0;
+    width: 17px;
+    height: 1px;
+    background-color: currentColor;
+  }
+</style>
+
+<div class="relative inline-block align-baseline">
+  <div class="icon-menu" aria-hidden="true" />
+</div>
