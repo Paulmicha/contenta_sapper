335af5230aa651f86a9c3225b78e83a8703a0d23
Paulmicha <paul.michalet@gmail.com>
2019-06-28 11:45:14 -0300

    feat: main menu nav links WIP

--- frontend/src/components/Logo.svelte
+++ frontend/src/components/Logo.svelte
@@ -1,11 +1,24 @@
 <script>
+  import language from "../stores/language.js";
+
+  // The link to homepage depends on currently active language.
+  // TODO [wip] rework implementation.
+  // @see src/components/LanguageSwitcher.svelte
+  let active_language = "";
+  const unsubscribe = language.subscribe(value => {
+    active_language = value;
+  });
+
+  // TODO fetch default language from Drupal to handle unprefixed homepage link.
+  const default_language = "en";
+
   // TODO get Drupal's translatable strings.
   const link_title = "Go to home page";
   const logo_alt_text = "Umami Food Magazine logo";
-
-  // The link to homepage depends on currently active language.
 </script>
 
-<a href="/" title={link_title}>
+<a
+  href={active_language == default_language ? '/' : '/' + active_language}
+  title={link_title}>
   <img src="logo.svg" alt={logo_alt_text} />
 </a>
--- frontend/src/components/Nav.svelte
+++ frontend/src/components/Nav.svelte
@@ -11,28 +11,34 @@
 
   // TODO get Drupal's translatable strings.
   const nav_label = "Primary";
+
+  // TODO [wip] implement transition for smaller viewports + hide it for
+  // larger ones.
 </script>
 
 <style>
-  button {
+  .nav-toggle {
     display: block;
     min-height: 1em;
     min-width: 1em;
   }
 </style>
 
-<nav aria-labelledby="primary-nav-label">
+<nav class="flex" aria-labelledby="primary-nav-label">
   <div id="primary-nav-label" hidden>{nav_label}</div>
-  <ul>
+  <ul class="flex mr-2">
     {#each menu_links as l}
-      <li>
-        <a class="font-serif {segment === l.path ? 'is-on' : ''}" href={l.href}>
+      <li class="mr-2">
+        <a
+          class="p-1 font-serif {segment === l.path ? 'is-on' : ''}"
+          href={l.href}>
            {l.label}
         </a>
       </li>
     {/each}
   </ul>
   <button
+    class="py-1 pl-1 nav-toggle"
     aria-label="Toggle the menu"
     on:click={e => {
       console.log(e);
